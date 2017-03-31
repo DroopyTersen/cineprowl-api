0aa7a3d0e66ab6300106aa2f1837ff9464f6188b
Andrew Petersen <andrew.petersen15@gmail.com>
2017-03-31 18:29:04 -0500

    switched to express

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-var restify = require('restify');
-var server = restify.createServer();
+var bodyParser = require("body-parser");
+var express = require("express");
 
+var env = process.env.NODE_ENV || 'dev';
+var config = { port: process.env.PORT || 4445 };
 
-server.use(restify.queryParser());
-server.use(restify.CORS());
-server.use(restify.jsonp());
-server.use(restify.bodyParser());
+var app = express();
+app.use(bodyParser.json());
+//Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-require("./src/routes").configure(server);
+require("./src/routes").configure(app);
 
 var startServer = function () {
-	var port = process.env.PORT || 4445;
-	var host = process.env.IP;
-	server.listen(port, host, function () {
-		console.log('REST API server listening on port ' + host + ":" + port);
-	});
+	app.listen(config.port, () => console.log("Server running on port " + config.port))
 }
-var streamer = require("./src/streamer");
-streamer.start(server);
+
 startServer();
